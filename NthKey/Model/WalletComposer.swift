7faf140f920299a2b2773eb32e400ce825d72f5f
Sjors Provoost <sjors@sprovoost.nl>
2020-02-16 17:50:29 +0100

    Add WalletComposer with announcements array

---
+++ NthKey/Model/WalletComposer.swift
@@ -0,0 +1,83 @@
+//
+//  WalletComposer.swift
+//  WalletComposer
+//
+//  Created by Sjors Provoost on 16/02/2020.
+//  Copyright © 2020 Purple Dunes. Distributed under the MIT software
+//  license, see the accompanying file LICENSE.md
+
+import Foundation
+import LibWally
+
+public struct WalletComposer : Codable {
+    
+    var announcements: [SignerAnnouncement]
+
+    public struct SignerAnnouncement: Codable {
+        private var fingerprint: Data
+        var name: String
+        var fingerprintString: String {
+            get { return fingerprint.hexString }
+        }
+
+        private enum CodingKeys : String, CodingKey {
+            case fingerprintString = "fingerprint"
+            case name
+        }
+        
+        init(fingerprint: Data, name: String) {
+            self.fingerprint = fingerprint
+            self.name = name
+        }
+
+        public init(from decoder:Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            name = try container.decode(String.self, forKey: .name)
+            let fingerprintString = try container.decode(String.self, forKey: .fingerprintString)
+
+            if fingerprintString.count != 8 {
+                throw DecodingError.dataCorruptedError(
+                    forKey:.fingerprintString,
+                    in: container,
+                    debugDescription: """
+                    Expected "\(fingerprintString)" to have 8 characters
+                    """
+                )
+            }
+    
+            guard let value = Data(fingerprintString) else {
+                throw DecodingError.dataCorruptedError(
+                    forKey: .fingerprintString,
+                    in: container,
+                    debugDescription: """
+                    Failed to convert an instance of \(Data.self) from "\(fingerprintString)"
+                    """
+                )
+            }
+            
+            if value.hexString != fingerprintString {
+                  throw DecodingError.dataCorruptedError(
+                      forKey:.fingerprintString,
+                      in: container,
+                      debugDescription: """
+                      "\(fingerprintString)" is not hex
+                      """
+                  )
+            }
+
+            fingerprint = value
+        }
+        
+        public func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            try container.encode(fingerprintString, forKey: .fingerprintString)
+            try container.encode(name, forKey: .name)
+        }
+    }
+    
+    public init?(us: Signer, signers: [Signer]) {
+        self.announcements = signers.map { signer in
+            return SignerAnnouncement(fingerprint: signer.fingerprint, name: us == signer ? "NthKey" : "")
+        }
+    }
+}
---
+++ NthKeyTests/WalletComposerTests.swift
@@ -0,0 +1,43 @@
+//
+//  WalletComposerTests.swift
+//  WalletComposerTests
+//
+//  Created by Sjors Provoost on 16/02/2020.
+//  Copyright © 2020 Purple Dunes. Distributed under the MIT software
+//  license, see the accompanying file LICENSE.md
+
+import XCTest
+import NthKey
+import LibWally
+
+class WalletComposerTests: XCTestCase {
+    var us: Signer?
+
+    override func setUp() {
+        let master1 = "xprv9s21ZrQH143K3QTDL4LXw2F7HEK3wJUD2nW2nRk4stbPy6cq3jPPqjiChkVvvNKmPGJxWUtg6LnF5kejMRNNU3TGtRBeJgk33yuGBxrMPHi"
+        
+        let path = BIP32Path("m/48'/0'/0'/2'")!
+        let multisigKey1 = try! HDKey(master1)!.derive(path)
+    
+        us = Signer(fingerprint: Data("3442193e")!, derivation: path, hdKey: multisigKey1, name: "NthKey")
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testAnnounceOurKey() {
+        let expected = #"""
+        {"announcements":[{"fingerprint":"3442193e","name":"NthKey"}]}
+        """#
+        let composer = WalletComposer(us: us!, signers: [us!])
+        XCTAssertNotNil(composer)
+        if (composer != nil) {
+            let encoder = JSONEncoder()
+            let encoded = try! encoder.encode(composer)
+            let json = String(data: encoded, encoding: .utf8)!
+            XCTAssertEqual(json, expected)
+        }
+    }
+
+}
