83a0a9d5f7362656b4a37170a512f148a0fbd781
Sjors Provoost <sjors@sprovoost.nl>
2020-11-26 15:49:41 +0100

    Display pubkey QR code

--- NthKey/View/SettingsView.swift
+++ NthKey/View/SettingsView.swift
@@ -9,6 +9,7 @@
 import Foundation
 import SwiftUI
 import Combine
+import CoreImage.CIFilterBuiltins
 
 struct SettingsView : View {
 
@@ -16,9 +17,32 @@ struct SettingsView : View {
 
     @State private var showMnemonic = false
     @State private var threshold = "2"
+    @State private var showPubKeyQR = false
 
     let settings = SettingsViewController()
+    
+    // For QR code
+    // https://www.hackingwithswift.com/books/ios-swiftui/generating-and-scaling-up-a-qr-code
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
 
+    func generateQRCode(from string: String) -> UIImage {
+        let data = Data(string.utf8)
+        filter.setValue(data, forKey: "inputMessage")
+
+        if let outputImage = filter.outputImage {
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimg)
+            }
+        }
+
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
+    
+    func togglePubKeyQR() {
+        self.showPubKeyQR = !self.showPubKeyQR
+    }
+    
     func loadCosignerFile(_ url: URL) {
         DispatchQueue.main.async() {
             self.appState.walletManager.loadCosignerFile(url)
@@ -85,11 +109,21 @@ struct SettingsView : View {
                     Text("Announce your key to your cosigners")
                 }
                 Button(action: {
+                    self.togglePubKeyQR()
+                }) {
+                    Text("QR")
+                }
+                if (self.showPubKeyQR) {
+                    Image(uiImage: generateQRCode(from: String(data: self.appState.walletManager.ourPubKey(), encoding: .utf8)!))
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200, height: 200)
+                }
+                Button(action: {
                     self.settings.exportPublicKey(data: self.appState.walletManager.ourPubKey())
                 }) {
                     Text("Save as JSON")
                 }
-                .disabled(!self.appState.walletManager.hasWallet)
                 Spacer()
                 VStack(alignment: .leading, spacing: 20.0) {
                     Text("Misc").font(.headline)
