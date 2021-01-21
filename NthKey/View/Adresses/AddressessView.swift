3933d2c0e4332aad9d6d92a464a3faa42af2e518
Ahmed Fathi <ahmedfathi680@gmail.com>
2021-01-21 20:44:16 +0100

    [WIP] Refactor AddressView.

---
+++ NthKey/View/Adresses/AddressessView.swift
@@ -0,0 +1,55 @@
+//
+//  AddressessView.swift
+//  AddressessView
+//
+//  Created by Fathi on 10/1/21.
+//  Copyright © 2021 Purple Dunes. Distributed under the MIT software
+//  license, see the accompanying file LICENSE.md
+
+import SwiftUI
+
+struct AddressessView: View {
+    
+    let walletManager: WalletManager
+    init(_ walletManager: WalletManager) {
+        self.walletManager = walletManager
+    }
+    
+    var body: some View {
+        
+        NavigationView {
+            
+            HStack {
+                VStack(alignment: .leading, spacing: 10.0) {
+                    
+                    if (self.walletManager.hasWallet) {
+                        List {
+                            ForEach(0..<1000) { i in
+                                AddressView(multisigAddress(for: i))
+                            }
+                        }
+                        
+                    } else {
+                        Text("Go to Settings to add cosigners")
+                    }
+                }
+            }
+            .navigationBarTitle("Address")
+        }
+    }
+    
+    func multisigAddress(for index: Int) -> MultisigAddress {
+        // TODO: PRE CREATE ALL OF THEM AND ONLY PASS BACK THE REQUESTED ONE
+        MultisigAddress(
+            threshold: UInt(self.walletManager.threshold),
+            receiveIndex: UInt(index),
+            network: self.walletManager.network
+        )
+    }
+}
+
+struct AddressessView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddressessView(AppState().walletManager)
+    }
+}
