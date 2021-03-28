c6a2592100d6fff44a338c7ee3096f7b00ebc692
Sergey Vinogradov <wins.konar@gmail.com>
2021-03-29 02:06:40 +0300

    add codesigner model, prepare for handle scans

--- NthKey/View/Settings/SettingsViewModel.swift
+++ NthKey/View/Settings/SettingsViewModel.swift
@@ -7,15 +7,50 @@
 //  license, see the accompanying file LICENSE.md
 
 import Foundation
+import CodeScanner
 
 final class SettingsViewModel {
     private let dataManager: DataManager
 
     let walletListModel: WalletListViewModel
+    let codeSignersModel: CodeSignersViewModel
 
     init(dataManager: DataManager) {
         self.dataManager = dataManager
 
         walletListModel = WalletListViewModel(dataManager: dataManager)
+        codeSignersModel = CodeSignersViewModel(dataManager: dataManager)
+    }
+
+    func handleScan(result: Result<String, CodeScannerView.ScanError>) {
+        // self.isShowingScanner = false
+        switch result {
+        case .success(let code):
+            DispatchQueue.main.async() { [weak self] in
+                guard let data = code.data(using: .utf8) else {
+                    // TODO: show error
+                    return
+                }
+                self?.dataManager.loadWalletUsingData(data) { result in
+                    // TODO: show error or success
+                }
+            }
+        case .failure(let error):
+            // TODO: show error
+            print("Scanning failed")
+            print(error)
+        }
     }
 }
+
+#if DEBUG
+extension SettingsViewModel {
+    static var mock: SettingsViewModel {
+        let model = SettingsViewModel(dataManager: DataManager.preview)
+        if let first = model.walletListModel.items.first {
+            model.walletListModel.selectedWallet = first
+        }
+        return model
+    }
+}
+#endif
