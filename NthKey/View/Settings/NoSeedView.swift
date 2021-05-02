df703f76a2a3c8f0f812a9f5fe59cb3291105876
Sergey Vinogradov <wins.konar@gmail.com>
2021-05-02 23:06:05 +0300

    move mnemonic work to separate view

--- NthKey/View/Settings/NoSeedView.swift
+++ NthKey/View/Settings/NoSeedView.swift
@@ -6,17 +6,13 @@
 //  Copyright © 2021 Purple Dunes. Distributed under the MIT software
 //  license, see the accompanying file LICENSE.md
 
-import Combine
 import SwiftUI
-import LibWally
 
 struct NoSeedView: View {
     
     @EnvironmentObject var appState: AppState
     
     @State private var enterMnemonnic = false
-    @State private var mnemonicInput = ""
-    @State private var validMnemonic = false
     
     var body: some View {
         VStack(alignment: .leading, spacing: 20.0) {
@@ -33,30 +29,7 @@ struct NoSeedView: View {
             }
 
             if self.enterMnemonnic {
-                TextField("battery staple horse...", text: $mnemonicInput)
-                    .keyboardType(.alphabet)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .autocapitalization(.none)
-                    .disableAutocorrection(true)
-                    .onReceive(Just(mnemonicInput)) { newValue in
-                        // TODO:
-                        // * check every word against BIP39Words
-                        // * suggest autocomplete for each
-                        let words: [String] = newValue.components(separatedBy: " ")
-                        if words.count == 12 || words.count == 16 || words.count == 24 {
-                            // TODO:
-                            // * make BIP39Mnemonic do the above check
-                            // * make isValid public
-                            self.validMnemonic = LibWally.BIP39Mnemonic(words) != nil
-                        } else {
-                            self.validMnemonic = false
-                        }
-                    }
-                Button("OK") {
-                    let words: [String] = self.mnemonicInput.components(separatedBy: " ")
-                    self.appState.walletManager.setEntropy(LibWally.BIP39Mnemonic(words)!.entropy)
-                }
-                .disabled(!self.validMnemonic)
+                EnterMnenonicView(model: EnterMnenonicViewModel(seedManager: appState.walletManager))
             }
         }
     }
